9850dd68239c9d3970703dcb5af2645a06398d93
hmtesser <hmtesser@gmail.com>
2024-11-05 23:03:13 -0300

    add: readcsv File using csvParser

---
+++ src/services/readcsv.ts
@@ -0,0 +1,18 @@
+import fs from 'fs';
+import csv from 'csv-parser'
+
+export const readCSV = (fileName:string): Promise<any[]> => {
+    return new Promise((resolve,reject) => {
+
+        const results:any[] = [];
+        fs.createReadStream(fileName).pipe(csv()).on('data', data => {
+            results.push(data);
+        })
+        .on('end', () => {
+            resolve(results);
+        })
+        .on('error', (error) => {
+            reject(error)
+        })
+    })
+}
