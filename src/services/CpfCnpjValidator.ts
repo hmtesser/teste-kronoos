3ebc2a67b43a30034e48cdecab11c05295e9d2d1
hmtesser <hmtesser@gmail.com>
2024-11-06 02:06:50 -0300

    feat: CPNJ validation added

--- src/services/CpfCnpjValidator.ts
+++ src/services/CpfCnpjValidator.ts
@@ -7,9 +7,9 @@ export function validate(value:string){
         } else {
             // verificação do CPF
             const cpfNineDigits = formattedValue.slice(0,9).split("").map(Number)
-            const firstDigit = verifyDigit(cpfNineDigits.reduce((sum, digit, index) => sum + digit * (10- index), 0));
+            const firstDigit = verifyCpfDigit(cpfNineDigits.reduce((sum, digit, index) => sum + digit * (10- index), 0));
             const cpfTenDigits = [...cpfNineDigits, firstDigit];
-            const secondDigit = verifyDigit(cpfTenDigits.reduce((sum, digit, index) => sum + digit * (11 -index),0));
+            const secondDigit = verifyCpfDigit(cpfTenDigits.reduce((sum, digit, index) => sum + digit * (11 -index),0));
 
             if(formattedValue[9] === firstDigit.toString() && formattedValue[10] === secondDigit.toString()){
                 return true;
@@ -21,22 +21,34 @@ export function validate(value:string){
     }else if(formattedValue.length == 14){
         const checkEqualCNPJ = formattedValue.split('').every((char) => char == formattedValue[0])
         if(checkEqualCNPJ){
+            console.log(formattedValue);
             return 'CNPJ inválido'
         }else{
             // verificação do CNPJ
-            const CNPJTwelveDigits = formattedValue.slice(0,12).split("").map(Number)
-            
+            const CNPJTwelveDigits = formattedValue.slice(0, 12).split("").map(Number);
+            const firstDigit = verifyCpfDigit(CNPJTwelveDigits.reduce((sum, digit, index) => sum + digit * [5,4,3,2,9,8,7,6,5,4,3,2][index], 0));
+        
+            // Calcula o segundo dígito verificador
+            const CPNJThirteenDigits = [...CNPJTwelveDigits, firstDigit];
+            const secondDigit = verifyCpfDigit(CPNJThirteenDigits.reduce((sum, digit, index) => sum + digit * [6,5,4,3,2,9,8,7,6,5,4,3,2][index], 0));
+        
+            if(formattedValue[12] == firstDigit.toString() && formattedValue[13] === secondDigit.toString()){
+                return true;
+            } else {
+                return false;
+            }
         }
     }else{
-        return 'CPF ou CNPJ inválido';
+        return 'CPF ou CNPJ incorreto';
     }
 }
 
 
-function verifyDigit(val:number): number {
+function verifyCpfDigit(val:number): number {
     const rest = val % 11;
     return rest <2 ? 0 : 11 - rest;
 }
 
-console.log(validate('11.111.111/1111-11'));
+
+console.log(validate('33.041.260/1443-28'));
 
